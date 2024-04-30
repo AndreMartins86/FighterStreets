e9190ae7a399168b4580c5ec882caac62b0df52d
AndreMartins86 <andre.mota86@hotmail.com>
2024-04-30 11:05:36 -0300

    bootstrap accordion

--- resources/views/painel/painel_torneio.blade.php
+++ resources/views/painel/painel_torneio.blade.php
@@ -1,9 +1,9 @@
 @extends('app')
 @section('conteudo')
-@push("meta")
-<meta name="csrf-token" content="{{ csrf_token() }}">    
-@endpush
-    @include('painel.layout.navpainel')    
+    @push('meta')
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    @endpush
+    @include('painel.layout.navpainel')
 
     <div class="container-fluid">
         <div class="row">
@@ -74,19 +74,6 @@
                     </div>
                 </div>
 
-                <div class="container-fluid">
-                    <div class="row my-2">
-                        <div class="col-12">
-                            <h5 id="res" style="display: none"></h5>
-                            <button class="btn btn-outline-danger" onclick="resetarDestaques()" title="Resetar destaques"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-octagon" viewBox="0 0 16 16">
-                                <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1z"/>
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                              </svg></button>
-
-                        </div>
-                    </div>
-                </div>
-
                 @if (session()->has('msg'))
                     <div class="alert alert-success">
                         <p>{{ session()->get('msg') }}</p>
@@ -96,37 +83,66 @@
                 <div class="container-fluid">
                     <div class="row my-2">
                         <div class="col-12">
-                            <table class="table table-striped" id="tabela" ondrop="dropDestaque(event)"
-                                ondragover="permitirDrop(event)">
-                                <tbody id="tabelaDrop">
-                                    @if ($destaques == false)
-                                    <tr id="titulo">
-                                        <th>Arraste aqui para adicionar destaques</th>
-                                    </tr>                                        
-                                    @else
-                                    <tr id="titulo">
-                                        <th>Arraste aqui para adicionar destaques</th>
-                                    </tr>  
-                                    @foreach ($destaques as $destaque)
-                                    <tr id="{{ $destaque->id }}" ondragstart="inicioArrastaDestaque(event)" ondragend="fimArrasta(event)" ondrop="ordenarDestaque(event)" ondragover="permitirDrop(event)" draggable="true">
-                                        <td>{{ $destaque->titulo }}</td>
-                                        <td>{{ $destaque->data }}</td>
-                                        <td>{{ $destaque->cidade }} - {{ $destaque->sigla }}</td>
-                                        <td>
-                                            <button type="button" title="Apagar destaque" class="btn btn-danger" onclick="apagarDestaque(this)"><svg
-                                                xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                                            </svg>
-                                            </button>
-                                        </td>
-                                    </tr>                                        
-                                    @endforeach                                                                       
-                                    @endif
-                                    
-                                </tbody>
-                            </table>
+                            <div class="accordion" id="accordion">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            Destaques
+                                        </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordion">
+                                        <div class="accordion-body">
+                                            <h5 id="res" style="display: none"></h5>
+                                                <button class="btn btn-outline-danger" onclick="resetarDestaques()"
+                                                    title="Resetar destaques"><svg xmlns="http://www.w3.org/2000/svg"
+                                                        width="16" height="16" fill="currentColor"
+                                                        class="bi bi-x-octagon" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1z" />
+                                                        <path
+                                                            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                                                    </svg></button><br>
+                                                    <table class="table table-striped" id="tabela" ondrop="dropDestaque(event)"
+                                                    ondragover="permitirDrop(event)">
+                                                    <tbody id="tabelaDrop">
+                                                        @if ($destaques == false)
+                                                            <tr id="titulo">
+                                                                <th>Arraste aqui para adicionar destaques</th>
+                                                            </tr>
+                                                        @else
+                                                            <tr id="titulo">
+                                                                <th>Arraste aqui para adicionar destaques</th>
+                                                            </tr>
+                                                            @foreach ($destaques as $destaque)
+                                                                <tr id="{{ $destaque->id }}" ondragstart="inicioArrastaDestaque(event)"
+                                                                    ondragend="fimArrasta(event)" ondrop="ordenarDestaque(event)"
+                                                                    ondragover="permitirDrop(event)" draggable="true">
+                                                                    <td>{{ $destaque->titulo }}</td>
+                                                                    <td>{{ $destaque->data }}</td>
+                                                                    <td>{{ $destaque->cidade }} - {{ $destaque->sigla }}</td>
+                                                                    <td>
+                                                                        <button type="button" title="Apagar destaque" class="btn btn-danger"
+                                                                            onclick="apagarDestaque(this)"><svg xmlns="http://www.w3.org/2000/svg"
+                                                                                width="16" height="16" fill="currentColor" class="bi bi-trash3"
+                                                                                viewBox="0 0 16 16">
+                                                                                <path
+                                                                                    d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                                                                            </svg>
+                                                                        </button>
+                                                                    </td>
+                                                                </tr>
+                                                            @endforeach
+                                                        @endif
+                                
+                                                    </tbody>
+                                                </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -145,25 +161,26 @@
                             <tbody id="tabelaDrag">
 
                                 @foreach ($campeonatos as $campeonato)
-                                    <tr ondragstart="inicioArrastaTabela(event)" ondragend="fimArrasta(event)" draggable="true" id="{{ $campeonato->id }}">
+                                    <tr ondragstart="inicioArrastaTabela(event)" ondragend="fimArrasta(event)"
+                                        draggable="true" id="{{ $campeonato->id }}">
                                         <td>{{ $campeonato->titulo }}</td>
                                         <td>{{ $campeonato->getData() }}</td>
                                         <td>{{ $campeonato->getLocal() }}</td>
                                         <td>
                                             <a
                                                 href="{{ route('painel-torneios.show', ['painel_torneio' => $campeonato->id]) }}"><button
-                                                    title="Detalhes" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                        height="16" fill="currentColor" class="bi bi-search"
-                                                        viewBox="0 0 16 16">
+                                                    title="Detalhes" class="btn btn-primary"><svg
+                                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                                         <path
                                                             d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                                                     </svg></button></a>
 
                                             <a
                                                 href="{{ route('painel-torneios.edit', ['painel_torneio' => $campeonato->id]) }}"><button
-                                                    title="Editar torneio" class="btn btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                        height="16" fill="currentColor" class="bi bi-pencil"
-                                                        viewBox="0 0 16 16">
+                                                    title="Editar torneio" class="btn btn-warning"><svg
+                                                        xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                                         <path
                                                             d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                                     </svg></button></a>
@@ -195,7 +212,7 @@
     </div>
 
     @include('painel.layout.footer')
-@push('drag')
-<script src="js/drag.js"></script>    
-@endpush
+    @push('drag')
+        <script src="js/drag.js"></script>
+    @endpush
 @endsection
